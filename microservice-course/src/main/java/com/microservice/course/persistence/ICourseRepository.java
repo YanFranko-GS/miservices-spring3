038ee721f2647c4ad63b59f802a89971539e155c
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-02 23:17:29 -0500

    feat: anadiendo el repository  del curso

--- microservice-course/src/main/java/com/microservice/course/persistence/ICourseRepository.java
+++ microservice-course/src/main/java/com/microservice/course/persistence/ICourseRepository.java
@@ -1,4 +1,10 @@
 package com.microservice.course.persistence;
 
-public interface ICourseRepository {
+import com.microservice.course.entity.Course;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICourseRepository extends CrudRepository<Course, Long> {
+
 }
