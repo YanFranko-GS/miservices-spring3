6854011a34c21b0b3cfb00cbfd04b9dd4336b0c8
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-02 22:07:11 -0500

    feat: se añadio el controller student

--- microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -1,4 +1,33 @@
 package com.microservice.student.controller;
 
+import com.microservice.student.Entity.Student;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveStudent(@RequestBody Student student) {
+        studentService.save(student);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllStudent(){
+        return ResponseEntity.ok(studentService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> finById(@PathVariable Long id){
+        return ResponseEntity.ok(studentService.findById(id));
+    }
 }
