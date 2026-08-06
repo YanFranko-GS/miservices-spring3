1055b5afd113dd923ea69510019ed3703d29c607
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-05 21:45:21 -0500

    feat: se cambio de 8090 a 8080 para el Gateway

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
+//se cambio el localhost:8090/api/student para que el gateway reconosca el port sino no reconoce la consulta y botaria error
+@FeignClient(name = "msvc-student", url = "localhost:8080/api/student")
 public interface StudentClient {
 
 
-    @GetMapping("/search-ny-course/{idCourse}")
+    @GetMapping("/search-by-course/{idCourse}")
     List<StudentDTO> findAllStudentByCourse(@PathVariable Long idCourse);
 }
